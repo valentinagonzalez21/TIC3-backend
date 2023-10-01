312d77050c238c5a0e071956ad050dd070efed3e
Clara <cecheverria@correo.um.edu.uy>
2023-10-01 20:21:22 -0300

    add cors to enable requests from frontend

--- src/app.js
+++ src/app.js
@@ -2,12 +2,15 @@ import express from 'express';
 import userArtistRoutes from './routes/userArtistRoutes.js';
 import userBusinessRoutes from './routes/userBussinessRoutes.js';
 import loginRoutes from './routes/loginRoutes.js'
+import cors from 'cors'
+
 
 
 const app = express();
 
 // middlewares
 app.use(express.json());
+app.use(cors());
 
 app.use(userArtistRoutes);
 app.use(userBusinessRoutes);
