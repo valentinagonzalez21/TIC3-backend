db3754ec40e3b281f978ce46a83d897d52be5fa5
valentinagonzalez21 <vgonzalez@correo.um.edu.uy>
2023-10-15 09:38:40 -0300

    get upcoming events of artist

--- src/controllers/artistController.js
+++ src/controllers/artistController.js
@@ -1,5 +1,7 @@
 import { Artist } from "../models/Artist.js";
-import { User } from "../models/User.js"
+import { User } from "../models/User.js";
+import { Event } from "../models/Event.js";
+import {Op} from 'sequelize';
 
 export const getArtists = async (req, res) => {
     try {
@@ -82,5 +84,37 @@ export const updateArtist = async (req, res) => {
     }
 
 
+}
+
+export const getUpcomingEventsFromArtist = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const artist = await Artist.findByPk(id);
+        if (artist === null) {
+            res.status(404).json({ message: "Artista no encontrado" });
+        } else {
+            let date = new Date() // today
+            let currentDay= String(date.getDate()).padStart(2, '0');
+            let currentMonth = String(date.getMonth()+1).padStart(2,"0");
+            let currentYear = String(date.getFullYear());
+            const today = currentYear + '-' + currentMonth + '-' + currentDay;
+
+            console.log(today)
+
+            const events = await Event.findAll({
+                where: {
+                   [Op.and]: [
+                    {artist_assigned_id: id},
+                    {date: {[Op.gte]: today}}
+                   ] 
+                },
+                order: [['date', 'ASC']], // los mas cercanos a hoy primero
+            });
+            res.status(200).json({ events: events });
+        }
+       
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
 }
 
--- src/routes/artistRoutes.js
+++ src/routes/artistRoutes.js
@@ -1,4 +1,4 @@
-import {createArtist, getArtists, getArtist, updateArtist} from '../controllers/artistController.js';
+import {createArtist, getArtists, getArtist, updateArtist, getUpcomingEventsFromArtist} from '../controllers/artistController.js';
 import {Router} from 'express';
 
 const router = Router();
@@ -7,6 +7,7 @@ router.get("/artists", getArtists);
 router.post("/artists", createArtist);
 router.get("/artists/:id", getArtist);
 router.put("/artists/:id", updateArtist);
+router.get("/artists/:id/events/upcoming", getUpcomingEventsFromArtist);
 
 
 export default router
